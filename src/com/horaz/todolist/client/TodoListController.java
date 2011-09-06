d2e4f44b6413cc705c9d6c6577a9c224348d69ab
Jonas Gehring <jonas.gehring@inovex.de>
2011-09-06 17:29:00 +0200

    start, empty horaz project

---
+++ src/com/horaz/todolist/client/TodoListController.java
@@ -0,0 +1,12 @@
+package com.horaz.todolist.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point
+ */
+public class TodoListController implements EntryPoint {
+	@Override
+	public void onModuleLoad() {
+	}
+}
